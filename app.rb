5375e2cf08c92c91d5632a18dbbe77396d2ef6c4
KeslieBox <kesliebox@gmail.com>
2021-02-24 14:29:37 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+
+       new = PigLatinizer.new
+       @piglatinized = new.piglatinize(params[:user_phrase])
+       
+        erb :new
+    end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,35 @@
+class PigLatinizer
+    attr_accessor :words
+
+    def initialize
+        @words = words
+    end
+
+
+    def piglatinize(words)
+      
+        words_array = words.split(" ")
+       
+        words_array.map do |word|
+            first_letter = word.slice(0)
+            word_new = word[1..-1]
+            second_letter = word_new.slice(0)
+    
+             if word[0].match(/[aeouiAEIOU]/)
+                word = "#{word}way"
+            elsif word[0].match(/[bcdfghjklmnpqrstvw]/) && word[1].match(/[bcdfghjklmnpqrstvw]/) && word[2].match(/[bcdfghjklmnpqrstvw]/)
+                con_clust_three = word[3..-1]
+                third_letter = word_new.slice(1)
+                word = "#{con_clust_three}#{first_letter}#{second_letter}#{third_letter}ay"
+            elsif word[0].match(/[bcdfghjklmnpqrstvw]/) && word[1].match(/[bcdfghjklmnpqrstvw]/)
+                con_clust_two = word[2..-1]
+                word = "#{con_clust_two}#{first_letter}#{second_letter}ay" 
+            elsif word[0].match(/[bcdfghjklmnpqrstvwxyz]/)
+                word = "#{word_new}#{first_letter}ay"
+                binding.pry 
+            end
+        end.join(" ")
+    end
+
+    
+end
